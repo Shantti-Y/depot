b17fde326ed3f7f3a670499af8d62e52768bccc7
Takahiro Yoshioka <hammpilot@gmail.com>
2016-09-25 13:10:16 +0900

    Store page created

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ApplicationRecord
 
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
-  validates :title, uniqueness: true
+  validates :title, uniqueness: true, length: { minimum: 10, message: "は10文字以上でなければなりません。" }
   validates :image_url, allow_blank: true, format: {
     with:     %r{\.(gif|jpg|png)\z}i,
     message: 'はGIF、JPG、PNG画像のURLでなければなりません。'
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -14,41 +14,55 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get products_url
     assert_response :success
+    assert_select 'table > tr', Product.count
   end
 
   test "should get new" do
     get new_product_url
     assert_response :success
+    assert_select '.field > input', 3
+    assert_select '.field > textarea', 1
   end
 
   test "should create product" do
+    products = Product.count
     assert_difference('Product.count') do
       post products_url, params: { product: @update }
     end
 
     assert_redirected_to product_url(Product.last)
+    follow_redirect!
+    assert_select '#main > p', 5
   end
 
   test "should show product" do
     get product_url(@product)
     assert_response :success
+    assert_select '#main > p', 5
   end
 
   test "should get edit" do
     get edit_product_url(@product)
     assert_response :success
+    assert_select '.field > input', 3
+    assert_select '.field > textarea', 1
   end
 
   test "should update product" do
     patch product_url(@product), params: { product: @update }
     assert_redirected_to product_url(@product)
+    follow_redirect!
+    assert_select '#main > p', 5
   end
 
   test "should destroy product" do
+    products = Product.count
     assert_difference('Product.count', -1) do
       delete product_url(@product)
     end
 
     assert_redirected_to products_url
+    follow_redirect!
+    assert_select 'table > tr', products - 1
   end
 end
